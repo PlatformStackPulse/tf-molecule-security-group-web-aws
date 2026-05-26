f3eda439cfa94ef5b1e68cc0f4538e01ed98edb3
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:28:54 +0100

    feat: implement tf-molecule-security-group-web-aws molecule

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "security_group_id" {
+  description = "ID of the web security group"
+  value       = module.security_group.id
+}
+
+output "security_group_arn" {
+  description = "ARN of the web security group"
+  value       = module.security_group.arn
 }
